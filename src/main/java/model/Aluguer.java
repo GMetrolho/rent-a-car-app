3bbeedd45907a59ec8adcf436da3e541518f1882
Metras1 <gmetrolho@gmail.com>
2026-05-12 20:59:35 +0100

    Adição das duas colunas relativas às datas

--- src/main/java/model/Aluguer.java
+++ src/main/java/model/Aluguer.java
@@ -1,8 +1,19 @@
 package model;
 
 import java.time.LocalDateTime;
+
 import enums.Status_Aluguer;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "aluguer")
@@ -13,9 +24,11 @@ public class Aluguer {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false)
-  private LocalDateTime dataReserva;
+  @Column(name = "data_reserva", nullable = false)
+  private LocalDateTime dataReserva;            
+  @Column(name = "data_levantamento")
   private LocalDateTime dataLevantamento;
+  @Column(name = "data_devolucao")
   private LocalDateTime dataDevolucao;
   @Column(name = "valor_final", precision = 10, scale = 2)
   private double valorFinalPrevisto;
